32d588c79d88c2866676b726bc6b653f652ffaa6
Huka <mr.kharhums@gmail.com>
2022-04-04 20:06:08 +0700

    Initial

---
+++ src/main/java/dev/huka/resumejgenerator/ResumeJgeneratorApplication.java
@@ -0,0 +1,13 @@
+package dev.huka.resumejgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeJgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeJgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/huka/resumejgenerator/ResumeJgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.huka.resumejgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeJgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
